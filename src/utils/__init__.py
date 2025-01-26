80f80c0579bb183d2eaeaa083762a3442d1be747
vladyoslav <vaifaer@gmail.com>
2025-01-27 00:01:34 +0300

    feat: classifier notebook

---
+++ src/utils/__init__.py
@@ -0,0 +1,3 @@
+from .utils import load_images_from_categories
+
+__all__ = ["load_images_from_categories"]
---
+++ src/utils/utils.py
@@ -0,0 +1,19 @@
+import os
+import cv2
+import numpy as np
+from typing import List, Tuple
+
+def load_images_from_categories(path: str, categories: List[str]) -> Tuple[List[np.ndarray], List[str]]:
+    images: List[np.ndarray] = []
+    labels: List[str] = []
+
+    for category in categories:
+        category_path = os.path.join(path, category)
+        for image_name in os.listdir(category_path):
+            image_path = os.path.join(category_path, image_name)
+            image = cv2.imread(image_path)
+            if image is not None:
+                images.append(image)
+                labels.append(category)
+
+    return images, labels
